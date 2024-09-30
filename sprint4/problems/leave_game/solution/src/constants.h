15060bca9c4e4747db2766a4c167ed4f1dfea487
artndx <artur.stepanyan.03@yandex.ru>
2024-09-30 20:31:25 +0300

    Add leave_game Fix 1

--- sprint4/problems/leave_game/solution/src/constants.h
+++ sprint4/problems/leave_game/solution/src/constants.h
@@ -3,4 +3,4 @@
 
 static const unsigned NUM_THREADS = std::thread::hardware_concurrency();
 
-static const char* DB_URL = std::getenv("DB_URL");
+static const char* DB_URL = std::getenv("GAME_DB_URL");
